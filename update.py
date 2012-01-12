1a85244594cb1ebfaf1387303993a202bca8111d
Your Name <you@example.com>
2012-01-12 01:19:11 +0000

    install on all

--- update.py
+++ update.py
@@ -5,12 +5,22 @@ import subprocess
 
 def remote_run(node, command):
     c = subprocess.Popen(['ssh', '-t', '-o', 'StrictHostKeyChecking=no', 'node%s.princeton.vicci.org' % node, '-X',] + command, stdout=subprocess.PIPE)
-    return c.communicate()
+    return c
 
 
 # install on all nodes
-other_nodes = [10] + range(12, 20)
-for n in other_nodes:
-    remote_run(n, 'sudo yum -y install vim git htop java-1.6.0-openjdk-devel libunwind.x86_64 libsqlite3x-devel.x86_64 && sudo yum -y groupinstall "Development Tools"'.split(' '))
-    #print remote_run(n, 'echo "bob"'.split(' '))
+outputs = []
+other_nodes = [10] + range(12, 71)
 
+def install_packages_on_all_nodes():
+    for n in other_nodes:
+        c = remote_run(n, 'sudo yum -y install vim git htop java-1.6.0-openjdk-devel libunwind.x86_64 libsqlite3x-devel.x86_64 && sudo yum -y groupinstall "Development Tools"'.split(' '))
+        #c = remote_run(n, 'echo "bob\n"'.split(' '))
+        outputs.append(c)
+
+    for output in outputs:
+        o,e = output.communicate()
+        print o, e, '\n'
+
+if __name__ == '__main__':
+    install_packages_on_all_nodes()
