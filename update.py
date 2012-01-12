63301c5c226293ecdd7ff6eb7022fc2f8509c0e8
Your Name <you@example.com>
2012-01-12 03:21:46 +0000

    update stuff

--- update.py
+++ update.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
+import sys
 import subprocess
-
-#other_nodes = [10] + range(12, 13)
-other_nodes = [10] + range(12, 71)
-
 
 
 def remote_run(node, command):
     c = subprocess.Popen(['ssh', '-t', '-o', 'StrictHostKeyChecking=no', 'node%s.princeton.vicci.org' % node, '-X',] + command, stdout=subprocess.PIPE)
     return c
 
-def run_on_all_nodes(command):
+def run_on_all_nodes(command, print_output=False):
     outputs = []
     for n in other_nodes:
         c = remote_run(n, command.split(' '))
@@ -21,7 +18,9 @@ def run_on_all_nodes(command):
 
     for output in outputs:
         o,e = output.communicate()
-        print e, '\n'
+        if print_output:
+            print o
+        print e, '\n\n'
 
 def install_packages_on_all_nodes():
     command = 'sudo yum -y install vim git htop java-1.6.0-openjdk-devel libunwind.x86_64 libsqlite3x-devel.x86_64 cppunit-devel.x86_64 cppunit.x86_64 && sudo yum -y groupinstall "Development Tools"'
@@ -41,6 +40,14 @@ def rsync_all():
     for output in outputs:
         o,e = output.communicate()
         print e, '\n'
+ 
+def clear_logs():
+    command = 'rm -rf /home/princeton_ram/mesos/work/*'
+    return run_on_all_nodes(command)
+
+def print_logs():
+    command = 'cat /home/princeton_ram/mesos/work/*/*/*/*/*/*/*/*/*'
+    return run_on_all_nodes(command, print_output=True)
 
 def symlink_java_shit():
     command = 'sudo ln -s /etc/alternatives/java_sdk_1.6.0/jre/lib/amd64/server/libjvm.so /usr/lib64/libjvm.so'
@@ -49,5 +56,21 @@ def symlink_java_shit():
 if __name__ == '__main__':
     #symlink_java_shit()
     #install_packages_on_all_nodes()
-    rsync_all()
+
+    if len(sys.argv) <= 1:
+        print 'What to update? rsync, clearlogs, printlogs'
+    else: 
+        todo = sys.argv[1:]
+
+        # by default, just do nodes 10-12, but add all70 to do all 70 nodes
+        other_nodes = [10] + range(12, 13)
+        if 'all70' in todo:
+            other_nodes = [10] + range(12, 71)
+
+        if 'rsync' in todo:
+            rsync_all()
+        if 'clearlogs' in todo:
+            clear_logs()
+        if 'printlogs' in todo:
+            print_logs()
 
